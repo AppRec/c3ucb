d6f1505c60c250b1ad9ea9514409c9d427ec43f6
wudi8126 <12250619@life.hkbu.edu.hk>
2016-12-22 11:31:19 +0800

    Add files via upload

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import math
 import random
 
 def getUCB(theta,x,beta,V):
-    x_Vnorm = np.dot(np.dot(x,np.linalg.inv(V)),np.transpose(x)) ** 1/2
+    x_Vnorm = np.dot(np.dot(x,np.linalg.inv(V)),np.transpose(x)) ** (1/2)
     UCB = min(np.dot(np.transpose(theta),np.transpose(x)) + beta * x_Vnorm, 1)
     return UCB
     
@@ -23,10 +23,10 @@ def update_stat(V,X,hist_X,hist_Y,W,lamb,delta,gama=1):
     newY = np.vstack((hist_Y,W))
     ishape = X.shape[1]
     tmpI = np.identity(ishape) * lamb
-    theta = np.dot(np.dot(np.linalg.inv(np.dot(np.transpose(nweX),newX) + tmpI),np.transpose(newX)),Y)
+    theta = np.dot(np.dot(np.linalg.inv(np.dot(np.transpose(newX),newX) + tmpI),np.transpose(newX)),newY)
     R = 1
     d = V.shape[0]
-    #beta = R * np.sqrt(math.log(np.linalg.det(V)/(lamb**d * delta**2))) + np.sqrt(lamb)
+    #beta = R * np.sqrt(math.log(np.linalg.det(V)) - d * math.log(lamb) - 2 * math.log(delta)) + np.sqrt(lamb)
     beta = R * np.sqrt(np.linalg.slogdet(V)[1] - d * np.log(lamb) -2 * np.log(delta)) + np.sqrt(lamb)
     #beta = R * np.sqrt(np.linalg.slogdet(V)[1] - idV - 2 * np.log(delta)) + np.sqrt(lamb)
     return newV,newX,newY,theta,beta
