c46ed6a25388a5ea62066d2750c629185b35b5b0
wudi8126 <12250619@life.hkbu.edu.hk>
2017-02-24 11:30:52 +0800

    Add files via upload

---
+++ extract_all_sess.py
@@ -0,0 +1,88 @@
+#l[0]<- uid, l[1]<- appid, l[2]<- date&time, l[3]<- oper_type, l[4]<- category, l[5]<- '', l[6]<- listid, l[7]<- position
+import sys
+import time
+
+processed_data = []
+processed_data2 = []
+optype_dict = {}
+app_dict = {}
+user_dict = {}
+
+mydate = 20161100
+record_counter = 0
+user_counter = 0
+start = time.clock()
+t=0
+days = 15
+for i in range(0,days):
+    #print 'Start at time: %.3f'%(time.clock()-start)
+    mydate += 1
+    #filename = '../DataFile/Data_randomHiad_' + str(mydate) + '.txt'
+    filename = './Data_randomHiad_' + str(mydate) + '.txt'
+    with open(filename,'r') as f:
+        data = f.readlines();
+        record_counter += len(data)
+        for line in data:
+            l = line.strip().split('\t')
+            #delete rows where position<3 && position>7 and category='HiAd'in list = 2
+            if l[4] != 'HiAd' or l[6] != '2' or int(l[7])<3 or int(l[7])>7:#HiAd
+                continue
+            #delete date&time, HiAd;
+            del l[2]
+            del l[3]
+            del l[3]
+            processed_data.append(l)
+        f.close()	
+print ("There are: ",record_counter,"rows")
+#print 'Session Loaded at: %.3f'%(time.clock()-start)
+for line in processed_data:
+    processed_data2.append(line)
+#print 'Operation filtering completed at: %.3f'%(time.clock()-start)
+
+#replace appID according to applist.txt
+with open("../applist.txt",'r') as f2:
+    #print '%.3f'%(time.clock()-start)
+    data = f2.readlines()
+    for line in data:
+        l = line.strip().split(',')
+        if l[0] not in app_dict:
+            app_dict[l[0]] = l[1]
+        #else:
+            #print("repeated app id")
+            #print "repeated app id"
+    f2.close()
+for i in range(0,len(processed_data2)):
+    processed_data2[i][1] = app_dict[processed_data2[i][1]]
+#print 'applist Loaded at: %.3f'%(time.clock()-start)
+    
+for line in processed_data2:
+    if line[0] not in user_dict: 
+        user_dict[line[0]] = user_counter
+        user_counter += 1
+for i in range(0,len(processed_data2)):
+    processed_data2[i][0] = user_dict[processed_data2[i][0]] 
+#print 'userlist Loaded at: %.3f'%(time.clock()-start)	
+print ('User amount is: ', user_counter)
+
+with open("../userlist.txt",'w') as f3:
+    for key in user_dict:
+        if int(time.clock()-start) / 30 > t:
+                t = int(time.clock()-start) / 30
+                #print '30 seconds left'
+        f3.write(key)
+        f3.write(',')
+        f3.write(str(user_dict[key]))
+        f3.write('\n')
+    #print 'userlist.txt outputed at: %.3f'%(time.clock()-start)
+    f3.close()
+	
+#print(processed_data2)
+with open("../session.txt", 'w') as f1:
+    #print '%.3f'%(time.clock()-start)
+    for line in processed_data2:
+        for e in line:
+            f1.write(str(e))
+            f1.write(',')
+        f1.write('\n')
+    f1.close()
+#print 'session.txt outputed at: %.3f'%(time.clock()-start)
