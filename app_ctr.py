8645fc9ccd56cfa01de209b068d95bd694e5f091
jkyu <JenkinsY94@users.noreply.github.com>
2017-02-24 10:51:47 +0800

    app_ctr sort apps result by ctr

--- app_ctr.py
+++ app_ctr.py
@@ -36,18 +36,7 @@ def main():
             else:
                 info.append(1)
             app_dict[aid] = info
-        # if row[1] in app_dict:
-            # app_dict[row[1]][0]+=1
-            # if(row[2]==11 or row[2]==10):
-                # app_dict[row[1]][1]+=1
-        # else:
-            # info=[]
-            # info.append(1)
-            # if(row[2]==61):
-                # info.append(0)
-            # else:
-                # info.append(1)
-            # app_dict[row[1]] = info
+        
             
         if row[-1] in position_dict:
             position_dict[row[-1]][0]+=1
@@ -63,20 +52,26 @@ def main():
             position_dict[row[-1]] = info
             
             
-    with open('../LogFile/app_ctr.txt','a') as f1:
-        f1.write('*'*20)
+    with open('../LogFile/app_ctr.txt','w') as f1:
+        f1.write('*'*20+'\n')
         for app in app_dict:
             app_ratio = float(app_dict[app][1])/app_dict[app][0]
             app_dict[app].append(app_ratio)
-            f1.write(str(app) + ',' + str(app_dict[app][0]) + ',' + str(app_dict[app][1]) + ',' + str(app_dict[app][2]))
+        app_dict = sorted(app_dict.items(), key = lambda x:x[1][2], reverse = True)
+        for app in app_dict:
+            #f1.write(str(app) + ',' + str(app_dict[app][0]) + ',' + str(app_dict[app][1]) + ',' + str(app_dict[app][2]))
+            f1.write(app)
             f1.write('\n')
         f1.close()
-    with open('../LogFile/pos_ctr.txt','a') as f2:
-        f2.write('*'*20)
+    with open('../LogFile/pos_ctr.txt','w') as f2:
+        f2.write('*'*20+'\n')
         for pos in position_dict:
             p_ratio = float(position_dict[pos][1])/position_dict[pos][0]
             position_dict[pos].append(p_ratio)
-            f2.write(str(pos) + ',' + str(position_dict[pos][0]) + ',' + str(position_dict[pos][1]) + ',' + str(position_dict[pos][2]))
+        position_dict = sorted(position_dict.items(), key = lambda x:x[1][2], reverse = True)
+        for pos in position_dict:  
+            #f2.write(str(pos) + ',' + str(position_dict[pos][0]) + ',' + str(position_dict[pos][1]) + ',' + str(position_dict[pos][2]))
+            f2.write(pos)
             f2.write('\n')
         f1.close()
     
